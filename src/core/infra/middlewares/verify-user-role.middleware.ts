090cbbc167e826df4be3b76af5353944c01accfb
Holeekey <danbrunosv@gmail.com>
2025-06-20 16:35:44 -0400

    fix(auth): :passport_control: allow endpoints for multiple roles

--- src/core/infra/middlewares/verify-user-role.middleware.ts
+++ src/core/infra/middlewares/verify-user-role.middleware.ts
@@ -1,10 +1,10 @@
 import { Role } from '../../../auth/app/models/credentials'
 import { getUserFromReq } from '../../../core/infra/utils/get-user-from-req'
 
-export const verifyUserRole = (role: Role) => {
+export const verifyUserRole = (...roles: Role[]) => {
   return async (req, res, next) => {
     const user = getUserFromReq(req)
-    if (user.role !== role) {
+    if (!roles.includes(user.role)) {
       return res.status(403).json({ message: 'Forbidden access' })
     }
     next()
